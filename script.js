99a2dfc47ed871bbe15b25789b84f1c059dd217d
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-05-25 18:26:26 +0100

    final modification

--- script.js
+++ script.js
@@ -1,60 +1,21 @@
 const body = document.querySelector('body')
-body.style.backgroundImage = 'url(./img/galaxy.gif)'
-body.style.display = 'flex'
-body.style.flexDirection = 'column'
-body.style.alignItems = 'center'
-body.style.justifyContent = 'center'
 body.style.fontFamily = 'Montserrat'
 
 const header = document.querySelector('header')
-header.style.marginBottom = '10px'
 
 const h1 = document.querySelector('h1')
-h1.style.textAlign = 'center'
-h1.style.color = '#ffffff'
-h1.style.fontSize = '30px'
-h1.style.textShadow = '1px 1px 1px #0000ff'
-h1.style.marginBottom = '50px'
 
 const input = document.querySelector('input')
-input.style.padding = '10px'
-input.style.outline = 'none'
-input.style.border = 'none'
-input.style.width = '200px'
-input.style.marginLeft = '80px'
 
 const select = document.querySelector('select')
-select.style.padding = '10px'
-select.style.outline = 'none'
-select.style.border = 'none'
-select.style.width = '150px'
-select.style.borderRadius = '3px'
-select.style.color = '#777676'
 
 const button = document.querySelector('button')
-button.style.padding = '12px'
-button.style.backgroundColor = '#777676'
-button.style.color = '#ffffff'
-button.style.outline = 'none'
-button.style.border = 'none'
-button.style.borderRadius = '3px'
-button.style.cursor = 'pointer'
 
 const main = document.querySelector('main')
-main.style.display = 'flex'
-main.style.alignItems = 'center'
-main.style.justifyContent = 'center'
-main.style.backgroundColor = 'rgba(255,255,255,0.1)'
-main.style.width = '90%'
 
 const flexContainer = document.querySelector('.flex-container')
-flexContainer.style.display = 'flex'
-flexContainer.style.alignItems = 'center'
-flexContainer.style.justifyContent = 'center'
-flexContainer.style.padding = '50px 0px 50px 20px'
  
 const flexItem = document.querySelector('.flex-item')
-flexItem.style.width = '70%'
 
 const planetImg = document.querySelector('.planet-image')
 planetImg.style.width = '100%'
@@ -87,8 +48,8 @@ function createDescriptionDiv(){
     descriptionDiv.style.fontSize = '30px'
     descriptionDiv.style.width = '100%'
     descriptionDiv.style.backgroundColor = 'rgba(255,255,255,0.1)'
-    descriptionDiv.style.padding = '0px 20px 5px 20px'
-    descriptionDiv.style.margin = '0 20px'
+    descriptionDiv.style.padding = '8px 20px 8px 20px'
+    descriptionDiv.style.margin = '0'
 
     return descriptionDiv
 }
